38080f56d7dd0d0e18c1ec02cc4196153b111ec2
HyeokJae <gurwo63@naver.com>
2023-06-08 18:43:47 +0900

    하다 만것들

--- FileNameManager/src/class_/Sorting.java
+++ FileNameManager/src/class_/Sorting.java
@@ -1,25 +1,96 @@
 package class_;
 
 import main.Data;
+
+import java.io.File;
+import java.util.Comparator;
+
 import inter.SortMode;
 
 public class Sorting implements SortMode
 {
+    
     public void Sort(Data db, int mod, boolean is_lower){
-        int index = 0;  //파일정렬 인덱스
+        int index = 0;  //비교하기 위한 파일 인덱스
+        int getSize = db.fileList.size();
+        SortComparatorLower sortNewer = new SortComparatorLower();
+        SortComparatorOlder sortOlder = new SortComparatorOlder();
+        SortComparatorUpper sortUpper = new SortComparatorUpper();
+        SortComparatorLower sortLower = new SortComparatorLower();
+
+        File fileTmp = null;
         switch(mod)
         {
             case 1:
-            //1. 이름순
-            break;
+                //이름순
+                if(is_lower)    //내림차순
+                {
+                    //만약에 아래 코드를 비교해서 두 파일 인덱스를 바꾼다....
+                    if(0 < sortUpper.compare(db.fileList.get(index), db.fileList.get(index+1)))
+                    {
+                    }
+                    
+                }
+                else   //오름차순
+                { 
+                    sortLower.compare(db.fileList.get(index), db.fileList.get(index+1));
+
+                }
+                break;
             case 2:
-            //2. 날짜순
+                //날짜순
+                if(is_lower)    //내림차순
+                {
+                        sortOlder.compare(db.fileList.get(index), db.fileList.get(index+1));
+                        index++;
+                }
+                else    //오름차순
+                {
+                        sortNewer.compare(db.fileList.get(index), db.fileList.get(index+1));
+                        index++;
+                }
             break;
             default:
             //돌아가기
             break;
         }
+    }
 
+    //오래된 순
+    class SortComparatorOlder implements Comparator<File>{
+
+        @Override
+        public int compare(File o1, File o2) {
+            if(o1.lastModified() > o2.lastModified()) return 1;
+            if(o1.lastModified() < o2.lastModified()) return -1;
+            return 0;
+        }
+    }
+    //최신 순
+    class SortComparatorNewer implements Comparator<File>{
+
+        @Override
+        public int compare(File o1, File o2) {
+            if(o1.lastModified() > o2.lastModified()) return -1;
+            if(o1.lastModified() < o2.lastModified()) return 1;
+            return 0;
+        }
+    }
+
+    class SortComparatorUpper implements Comparator<File>{
         
+        @Override
+        public int compare(File a, File b)
+        {
+            return a.getName().compareTo(b.getName());
+        }
+    }
+    class SortComparatorLower implements Comparator<File>{
+        
+        @Override
+        public int compare(File a, File b)
+        {
+            return a.getName().compareTo(b.getName()) * -1;
+        }
     }
 }
